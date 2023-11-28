0c209041959a9c618021ae700024c98c5dbda24e
Joel Hutchinson <hutchy100@live.co.uk>
2023-11-28 23:39:26 +0000

    Initialized SpringBoot project.

---
+++ novachess/src/main/java/com/novachess/NovachessApplication.java
@@ -0,0 +1,13 @@
+package com.novachess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovachessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovachessApplication.class, args);
+	}
+
+}
---
+++ novachess/src/test/java/com/novachess/NovachessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novachess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovachessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
