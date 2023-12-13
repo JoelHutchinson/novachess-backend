9a6d8630ec5346e6c516b723c3b4746160d775d0
Joel <jxh1244@student.bham.ac.uk>
2023-12-13 15:30:07 +0000

    Moved db username and password to secret properties file.

--- novachess/src/main/java/com/novachess/NovachessApplication.java
+++ novachess/src/main/java/com/novachess/NovachessApplication.java
@@ -2,8 +2,10 @@ package com.novachess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secret.properties")
 public class NovachessApplication {
 
 	public static void main(String[] args) {
