b755a0bf4dc7450f444604efe3cb7738bfa1986c
Joel <jxh1244@student.bham.ac.uk>
2023-12-13 14:01:45 +0000

    Added secret file to gitignore.

--- novachess/src/main/java/com/novachess/Puzzle.java
+++ novachess/src/main/java/com/novachess/Puzzle.java
@@ -4,13 +4,14 @@ import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
 @Entity
 class Puzzle {
 
-	private @Id @GeneratedValue Long id;
+	private @Id @GeneratedValue(strategy=GenerationType.AUTO) Long id;
 	private String fen;
 	private String moves;
 	private int rating;
